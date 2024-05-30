bae36b3c4320d87aeb0f9466091b3be6c4bd251c
ralfsbrutans <ralfs.brutans@gmail.com>
2024-05-30 16:21:41 +0200

    trying smth

--- feed_text_roberta.py
+++ feed_text_roberta.py
@@ -5,16 +5,11 @@ Created on Thu May 30 14:00:40 2024
 
 @author: RalfsArvids123
 """
-
 import json
-
-import torch
-from transformers import AutoModelForQuestionAnswering, AutoTokenizer, pipeline
+from transformers import pipeline
 
 # Loading the RoBERTa-base model for question answering
 model_name = "timpal0l/mdeberta-v3-base-squad2"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForQuestionAnswering.from_pretrained(model_name)
 
 
 # Function for reading JSONL file
@@ -23,42 +18,25 @@ def read_jsonl(file_path):
         lines = file.readlines()
         return [json.loads(line) for line in lines]
 
-
-def get_yes_or_no_answer(context, question):
-    inputs = tokenizer(
-        question, context, add_special_tokens=True, return_tensors="pt"
-    )
-    print("Tokenized Inputs:", inputs)
-
-    outputs = model(**inputs)
-    print("Model Outputs:", outputs)
-
-    answer_start = torch.argmax(outputs.start_logits)
-    answer_end = torch.argmax(outputs.end_logits)
-
-    if answer_start <= answer_end:
-        answer = tokenizer.decode(
-            inputs.input_ids[0][answer_start : answer_end + 1]
-        )
-        print("Raw Answer:", answer)
-        if answer in ["<s>", "</s>", "<pad>", ""]:
-            return "no answer"
-    else:
-        answer = ""
-
-    return answer.lower().strip()
-
-
 def get_answer(context, question):
-    qa_model = pipeline("question-answering", model_name)
-    QA_input = {"question": question, "context": context}
+    qa_model = pipeline("question-answering", model=model_name)
     return qa_model(question=question, context=context)["answer"]
 
+def interpret_answer(answer):
+    affirmative_words = {"yes", "sure", "indeed", "affirmative", "correct", "true"}
+    negative_words = {"no", "not", "none", "negative", "false"}
+
+    answer_tokens = set(answer.lower().split())
+
+    if affirmative_words & answer_tokens:
+        return "yes"
+    elif negative_words & answer_tokens:
+        return "no"
+    else:
+        return "no"
 
 # Read the prompts and context
-data = read_jsonl(
-    "prompts/english/shortened_length/explicit_shortened_english.jsonl"
-)
+data = read_jsonl("prompts/english/shortened_length/explicit_shortened_english.jsonl")
 
 # Process the prompts for answers
 for item in data:
@@ -68,7 +46,8 @@ for item in data:
     question = "Only answering with yes or no, " + sentences[-1].strip()
 
     # Get the answer before saving it
-    answer = get_answer(context, question)
+    raw_answer = get_answer(context, question)
+    answer = interpret_answer(raw_answer)
 
     item["context"] = context
     item["question"] = question
@@ -81,3 +60,4 @@ with open("output/exp_eng_short.jsonl", "w", encoding="utf-8") as f:
         f.write("\n")
 
 print("Answers have been added to 'output/exp_eng_short.jsonl'.")
+
