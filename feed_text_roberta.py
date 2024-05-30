e2c5d89c3baa21a8ee16531ee5af0966e5ba2f78
ralfsbrutans <ralfs.brutans@gmail.com>
2024-05-30 15:22:53 +0200

    add context,q to output file

--- feed_text_roberta.py
+++ feed_text_roberta.py
@@ -25,13 +25,7 @@ def read_jsonl(file_path):
 
 
 # Function to get answer (modified)
-def get_yes_or_no_answer(text):
-    sentences = text.split(".")
-    context = ".".join(sentences[:-1])  # Everything before the last sentence
-    question = (
-        "only answering with yes or no: " + sentences[-1].strip()
-    )  # Last sentence
-
+def get_yes_or_no_answer(context, question):
     inputs = tokenizer(
         question, context, add_special_tokens=True, return_tensors="pt"
     )
@@ -55,10 +49,18 @@ data = read_jsonl(
     "prompts/english/shortened_length/explicit_shortened_english.jsonl"
 )
 
+
 # Process the prompts for answers
 for item in data:
     text = item["filled_template"]
+    sentences = text.split(".")
+    context = ".".join(sentences[:-1])  # Everything before the last sentence
+    question = (
+        "only answering with yes or no: " + sentences[-1].strip()
+    )  # Last sentence
     answer = get_yes_or_no_answer(text)
+    item["context"]= context
+    item["question"]=question
     item["answers"] = answer  # Add answer to the item
 
 # Save the modified data back to the same file
